a99467763048bb958ef750e59b9f357f3d797d3d
not-Karot <52074617+not-Karot@users.noreply.github.com>
2022-06-15 13:23:10 +0200

    added deps

--- src/CPDTGW3D8a.jl
+++ src/CPDTGW3D8a.jl
@@ -3,7 +3,7 @@ module CPDTGW3D8a
     using LinearAlgebraicRepresentation
     using IntervalTrees
     using NearestNeighbors
-    using Triangle
+    #using Triangle
     using Triangulate
 	using SparseArrays
 	using LinearAlgebra
